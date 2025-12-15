35c0dac00847fbcfd4d93c8b1e7b54424a9455ec
omorfarukdeveloper24 <omorf4662@gmail.com>
2025-12-15 18:15:24 +0600

    update

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -262,7 +262,7 @@ public function testApi() {
     public function miniads(Request $request)
     {
         return "okk"; 
-        
+        return "NOt OKK";
         $member = Auth::guard('member')->user();
     
         if (!$member) {
