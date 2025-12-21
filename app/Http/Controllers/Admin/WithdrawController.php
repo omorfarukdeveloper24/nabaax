d76f4d49bdf5cc181ca0f583265c19d7157174de
omorfarukdeveloper24 <omorf4662@gmail.com>
2025-12-21 20:20:10 +0600

    str update

--- app/Http/Controllers/Admin/WithdrawController.php
+++ app/Http/Controllers/Admin/WithdrawController.php
@@ -10,7 +10,8 @@ use Carbon\Carbon;
 use Illuminate\Support\Facades\Session;
 use Brian2694\Toastr\Facades\Toastr;
 use Illuminate\Support\Facades\Auth;
-use DB;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class WithdrawController extends Controller
 {
