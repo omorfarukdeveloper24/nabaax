1424a73c567421d98169a113948223b413a35600
omorfarukdeveloper24 <omorf4662@gmail.com>
2025-12-22 14:16:13 +0600

    app c

--- config/app.php
+++ config/app.php
@@ -186,6 +186,7 @@ return [
          * Package Service Providers...
          */
         Intervention\Image\ImageServiceProvider::class,
+        Spatie\GoogleCloudStorage\GoogleCloudStorageServiceProvider::class,
 
         /*
          * Application Service Providers...
