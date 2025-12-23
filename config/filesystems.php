8466e8ff737f38e3895afdcca7c352cd240cab72
omorfarukdeveloper24 <omorf4662@gmail.com>
2025-12-23 13:25:24 +0600

    nnn

--- config/filesystems.php
+++ config/filesystems.php
@@ -59,11 +59,7 @@ return [
         'gcs' => [
             'driver' => 'gcs',
             'project_id' => env('GCS_PROJECT_ID'),
-            'key_file' => base_path(env('GCS_KEY_FILE')), 
             'bucket' => env('GCS_BUCKET'),
-            'path_prefix' => env('GCS_PATH_PREFIX', null),
-            'storage_api_uri' => env('GCS_STORAGE_API_URI', null),
-            
         ],
 
     ],
