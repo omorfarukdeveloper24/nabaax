1c895757f35a47dd24dd12a049b094c6a462b727
omorfarukdeveloper24 <omorf4662@gmail.com>
2025-12-24 20:08:58 +0600

    his

--- resources/views/backEnd/nb65vartex/dashboard.blade.php
+++ resources/views/backEnd/nb65vartex/dashboard.blade.php
@@ -252,7 +252,7 @@ a { text-decoration: none; }
             <div class="sc-admin-dashboard">
 
               <a href="#"><div class="sc-admin-card">
-                  <div class="sc-admin-icon bg-blue"><i data-feather="database"></i>  </div>
+                  <div class="sc-admin-icon bg-blue"><i class="fa-solid fa-money-check-dollar"></i>  </div>
                   <div class="school-admin-details">
                     <h3> {{ $total_balance }} </h3>
                     <p>All Balance</p>
@@ -268,7 +268,7 @@ a { text-decoration: none; }
               </div></a>
 
               <a href="#"><div class="sc-admin-card">
-                  <div class="sc-admin-icon bg-purple"><<i data-feather="download"></i></div>
+                  <div class="sc-admin-icon bg-purple"><i data-feather="download"></i></div>
                   <div class="school-admin-details">
                     <h3> {{ $total_deposit }} </h3>
                     <p>Total Deposit</p>
