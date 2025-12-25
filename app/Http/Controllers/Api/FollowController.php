434f1ba42ec2905649eef029c2343e5d4885a98f
omorfarukdeveloper24 <omorf4662@gmail.com>
2025-12-25 12:30:11 +0600

    update member

--- app/Http/Controllers/Api/FollowController.php
+++ app/Http/Controllers/Api/FollowController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Follow;
+use App\Models\Member;
 use App\Models\FollowBoost;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
