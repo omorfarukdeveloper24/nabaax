45cb3d1f76a896d8c03d8243013e4687f5c3ba68
omorfarukdeveloper24 <omorf4662@gmail.com>
2026-02-19 15:01:06 +0600

    mon update 9

--- app/Models/Earning.php
+++ app/Models/Earning.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Earning extends Model
 {
-    //
+    // এই অংশটুকু যোগ করুন
+    protected $fillable = [
+        'member_id',
+        'amount',
+        'new_views',
+        'new_watch_time',
+        'earning_date'
+    ];
 }
