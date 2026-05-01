e0733da0098ed0ea79cc29524a2dfa067518b321
omorfarukdeveloper24 <omorf4662@gmail.com>
2026-05-01 18:40:04 +0600

    error log check 02

--- app/Jobs/SendLikeNotificationJob.php
+++ app/Jobs/SendLikeNotificationJob.php
@@ -23,8 +23,6 @@ class SendLikeNotificationJob implements ShouldQueue
 
     public function handle(): void
     {
-        $this->undefinedFunctionCall();
-        
         $title = $this->reactionType === 'like' ? 'New Like 👍' : 'New Dislike 👎';
         $body  = "{$this->likerName} reacted to your post.";
 
